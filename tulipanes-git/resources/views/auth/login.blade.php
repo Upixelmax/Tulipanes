5fa3596c028db045575a1722b9260526a9ae0137
Crlsljndr <carlosalejandro-hotmail.cl>
2022-11-27 17:43:44 -0300

    actualizacion2711

--- tulipanes-git/resources/views/auth/login.blade.php
+++ tulipanes-git/resources/views/auth/login.blade.php
@@ -1,55 +1,37 @@
 
-<!DOCTYPE html>
-<html>
+
+
+
 <head>
-
-	<meta charset="utf-8">
-	<meta name="author" content="B8">
-	<meta name="description" content="Pagina Los Tulipanes">
-	<meta name="keywords" content="Practica, html, desarrollo web">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-	<title>Tulipanes</title>
-	<link rel="stylesheet" href="{{url('assets/css/estilo.css')}}">
-	<link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
-
+  <meta charset="utf-8">
+  <title>Tulipanes</title>
+  <base href="/tulipanes.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="#">
 </head>
+<div class="wrapper">
+<link rel="stylesheet" href="/css/login.css">
+<link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
+    <div class="body">
+        <img class="logo" src="/tulipanes.png" alt="Login">
+        <h1> BIENVENIDO</h1>
 
-<body>
+        <form action="/login" method="POST" >
+		@csrf
+            <div class="form-floating mb-3">
+                <input type="text" name="username" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Usuario</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Contraseña</label>
+              </div>
 
-	<!--===================================
-	LOGO
-	====================================-->
-<header id="nav" class="row">
-	<div class="col-12">
-		<h1>Los Tulipanes</h1>
-	</div>
-</header>
+              <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary btn-lg" type="button">INGRESAR</button>
 
-<div class="row">.</div>
-<div class="row">.</div>
-<div class="row">.</div>
-<div class="row">.</div>
-<div class="row">.</div>
+              </div>
 
-	<!--===================================
-	LOGIN
-	====================================-->
-<section  class="row">
-	<div class="col-4"></div>
-	<div id="login" class="col-4">
-		<h1>Ingreso</h1>
-			<form action="/login" method="POST">
-			@csrf
-				<input type="text" name="username" placeholder="Usuario">
-				<input type="password" name="password"placeholder="Contraseña">
-				<input type="submit" value="Enviar">
-			</form>
-	</div>
-	<div class="col-4"></div>
-</section>
-
-</body>
-<footer>Los Tulipanes &#174;</footer>
-</html>
+        </form>
+    </div>
+</div>
--- tulipanes-git/resources/views/home/index.blade.php
+++ tulipanes-git/resources/views/home/index.blade.php
@@ -1,72 +1,76 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-
-	<meta charset="utf-8">
-	<meta name="author" content="B8">
-	<meta name="description" content="Pagina Los Tulipanes">
-	<meta name="keywords" content="Practica, html, desarrollo web">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-	<title>Tulipanes</title>
-	<link rel="stylesheet" href="{{url('assets/css/estilo.css')}}">
-	<link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,300&display=swap" rel="stylesheet"> 
-
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicio</title>
+  <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
+  <link rel="stylesheet" href="/css/home.css">
 </head>
-
 <body>
 
-	<!--===================================
-	LOGO y CERRAR SESIÓN
-	====================================-->
-<header id="nav" class="row">
-	<div class="col-3">
-		<a>Los Tulipanes</a>
-	</div>
-	<div class="col-3"></div>
-	<div class="col-3"></div>
-	<div class="col-3" >
-		<button id="drop"class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    	{{auth()->user()->username}}
-  		</button>
-  		 <ul class="dropdown-menu">
-	    <li><a class="dropdown-item" href="/logout">Cerrar Sesión</a></li>
-	  	</ul>
-	</div>
-</header>
+  <main>
+    <h1>Inicio</h1>
 
-<div class="row">.</div>
-<div class="row">.</div>
-<div class="row">.</div>
-
-<div class="row">
-	<aside id="opciones" class="col-3">
-		<h3>Administrar</h3>
-		@auth
-		<button class="administracion" onclick="window.location.href='/departamento'">Departamentos</button><br>
-		<button class="administracion" onclick="window.location.href='/categoria'">Categorias</button><br>
-		<button class="administracion" onclick="window.location.href='/producto'">Producto</button><br>
-		<button class="administracion" onclick="window.location.href='/movimiento'">Movimientos</button><br>
-		<button class="administracion" onclick="window.location.href='/historial'">Historial</button><br>
-		<button class="administracion" onclick="window.location.href='/register'">Registrar</button><br>
-		@endauth
-	</aside>
-	<aside class="col-8" id="bienvenido">
-		<h1>Bienvenid@ {{auth()->user()->username}} </h1>
-	</aside>
-</div>
-
-
-
-
-
-<footer>Los Tulipanes &#174;</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <div class="container__box">
+      <div class="box">
+		<a href="#">
+			<i class="lni lni-agenda"></i>
+			<h5>Inventario</h5>
+			<h4>Inventario</h4>
+		</a>
+      </div>
+      <div class="box">
+		<a href="/producto">
+			<i class="lni lni-cart-full"></i>
+			<h5>Producto</h5>
+			<h4>Producto</h4>
+		</a>
+        
+      </div>
+      <div class="box">
+        <i class="lni lni-grid-alt"></i>
+        <h5>Categoria</h5>
+        <h4>Categoria</h4>
+      </div>
+      <div class="box">
+        <i class="lni lni-apartment"></i>
+        <h5>Departamento</h5>
+        <h4>Departamento</h4>
+      </div>
+      <div class="box">
+        <i class="lni lni-customer"></i>
+        <h5>Empleados</h5>
+        <h4>Empleados</h4>
+      </div>
+      <div class="box">
+        <i class="lni lni-folder"></i>
+        <h5>Historial</h5>
+        <h4>Historial</h4>
+      </div>
+      <div class="box">
+        <i class="lni lni-files"></i>
+        <h5>Facturas</h5>
+        <h4>Facturas</h4>
+      </div>
+      <div class="box">
+        <i class="lni lni-service"></i>
+        <h5>Recetas</h5>
+        <h4>Recetas</h4>
+      </div>
+      <div class="box">
+        <i class="lni lni-map-marker"></i>
+        <h5>Relleno 1</h5>
+        <h4>Relleno 1</h4>
+      </div>
+      <div class="box">
+        <i class="lni lni-reload"></i>
+        <h5>Relleno 2</h5>
+        <h4>Relleno 2</h4>
+      </div>
+    </div>
+  </main>
+  
 </body>
 </html>
-
-
